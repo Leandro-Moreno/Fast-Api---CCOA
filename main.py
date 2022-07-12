69f1f9bd2ffce20990b45c5929c1a8b9901f0bfe
Leandro Moreno <123857@gmail.com>
2022-07-11 19:35:04 -0500

    Update main.py

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ async def say_hello(name: str):
 async def tmodels(data: object = Form()):
     return data
 
-@app.get("/model")
+@app.post("/model")
 async def models(
         LCIIU: str,
         desc_organizacion: str,
